80d36515103798d66d18d90744fb93df58b96242
Mark Sorel <mark.sorel@dfw.wa.gov>
2026-02-03 14:15:15 -0800

    updating 2026 forecast

--- functions/install_or_load_pack.r
+++ functions/install_or_load_pack.r
@@ -1,7 +1,21 @@
 #function to install or load packages
-install_or_load_pack <- function(pack){
-  create.pkg <- pack[!(pack %in% installed.packages()[, "Package"])]
-  if (length(create.pkg))
-    install.packages(create.pkg, dependencies = TRUE,repos = "http://cran.us.r-project.org")
-  sapply(pack, require, character.only = TRUE)
+# install_or_load_pack <- function(pack){
+#   create.pkg <- pack[!(pack %in% installed.packages()[, "Package"])]
+#   if (length(create.pkg))
+#     install.packages(create.pkg, dependencies = TRUE,repos = "http://cran.us.r-project.org")
+#   sapply(pack, require, character.only = TRUE)
+# }
+
+install_or_load_pack <- function(packages) {
+  for (pkg in packages) {
+    # install only if missing
+    if (!requireNamespace(pkg, quietly = TRUE)) {
+      install.packages(pkg, dependencies = NA, repos = "https://cloud.r-project.org")
+    }
+    # always load
+    suppressPackageStartupMessages(
+      library(pkg, character.only = TRUE)
+    )
+  }
+  invisible(NULL)
 }
--- project_ersst.r
+++ project_ersst.r
@@ -1,16 +1,15 @@
-pacman::p_load(tidyverse,reshape2,dataRetrieval,lubridate,parallel,foreach,doParallel,rnoaa,MARSS)
+pacman::p_load(tidyverse,reshape2,dataRetrieval,lubridate,parallel,foreach,doParallel,rnoaa,MARSS,buoydata)
 source(here::here("functions/get_ersst_v5_new.r"))
-source(here::here("functions/getbuoydata.r"))
+source(here::here("functions/getbuoydata26.r"))
 
 
 
 
-
-years<-c(2010:2025)
-buoylist<-c("46050","46041","46211","46087","46206")
+years<-c(2010:2026)
+buoylist<-c("46050","46041","46211","46087")#,"46206")
 
 if(!file.exists(here::here("data/buoy_data.csv"))){
-  buoy_data <- getbuoydata(buoylist = buoylist,years=years)
+  buoy_data <- getbuoydata26(buoylist = buoylist,years=years)
   write.csv(buoy_data,here::here("data/buoy_data.csv"),row.names=F)
 }else{
   buoy_data<-read_csv(here::here("data/buoy_data.csv"))
@@ -75,9 +74,9 @@ marss_dat<-marss_dat%>%
   summarise(meanSST=mean(value))
 
 
-SST1<-get_ersst_v5_new(years=c(2010,2025),
+SST1<-get_ersst_v5_new(years=c(2010,2026),
                            data.dir=here::here("data"),
-                           ncfilename="sst.mnmean_1_23_2025.nc",
+                           ncfilename="sst.mnmean_1_27_2026.nc",
                            latrange=c(44,50),
                            lonrange=c(-125,-120)
 )%>%
@@ -91,7 +90,7 @@ SST1<-get_ersst_v5_new(years=c(2010,2025),
 
 marss_dat2<-marss_dat%>%
   bind_rows(SST1)%>%
-  filter(year < 2025 | month <2)%>%
+  filter(year < 2026 | month <2)%>%
   pivot_wider(names_from = buoyid,values_from = meanSST,id_cols=c("year","month"))
 
   
@@ -138,7 +137,7 @@ ggplot(results%>%
             
 write.csv(results%>%
             filter(year==year(Sys.Date())),
-          here::here("data/January_ERSST_v5_2025.csv"),row.names = F
+          here::here("data/January_ERSST_v5_2026.csv"),row.names = F
           )
 
          
